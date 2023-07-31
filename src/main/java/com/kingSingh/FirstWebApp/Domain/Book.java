4080e7255cd460d31bd2598d4962b0e7b9056e17
Anil Patamber <anilpatamber@gmail.com>
2023-07-31 23:47:03 +0530

    added Author and Book classses

---
+++ src/main/java/com/kingSingh/FirstWebApp/Domain/Book.java
@@ -0,0 +1,76 @@
+package com.kingSingh.FirstWebApp.Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+    private String name;
+    private String isbn;
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void SetBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        return getId() == book.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (getId() ^ (getId() >>> 32));
+    }
+}
