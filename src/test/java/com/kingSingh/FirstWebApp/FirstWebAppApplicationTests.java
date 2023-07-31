47121450edb75bb707d8d3df2c4b5f3f903129ba
Anil Patamber <anilpatamber@gmail.com>
2023-07-31 23:39:32 +0530

    My First Commit

---
+++ src/test/java/com/kingSingh/FirstWebApp/FirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingSingh.FirstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
